e5ab0d23b5a84eefb55e4638d0e6e90e4c7db633
Derek Morey <derek@derek.o.morey@gmail.com>
2025-05-26 21:29:36 -0400

    Got some basic stuff working

---
+++ makefile
@@ -0,0 +1,22 @@
+CC := gcc
+CFLAGS := -MMD -Wall -Wextra -pedantic -std=c11
+DEBUG_FLAGS := -ggdb
+
+SRC := $(wildcard src/*.c)
+OBJ := $(SRC:%.c=%.o)
+DEP := $(OBJ:%.o=%.d)
+BUILD := build
+
+EXE := $(BUILD)/repl
+
+all: debug
+
+debug: CFLAGS += $(DEBUG_FLAGS)
+debug: $(EXE)
+
+$(EXE): $(OBJ)
+	$(CC) -o $@ $^
+
+.PHONY: clean
+clean:
+	$(RM) $(OBJ) $(DEP) $(EXE)
--- src/main.c
+++ src/main.c
@@ -1,14 +1,84 @@
+/*
+ * Wrap a command in an interactive shell
+ * Receommend wrapping this program with rlwrap for command history other nice things
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define BUF_SIZE 256
 
 int
 main(int argc, char **argv) {
 	
-	if (argc == 0) {
-		printf("Error: no command given");
-		printf("Usage: repl <command> (<default> <prompt>)");
+	if (argc < 2) {
+		printf("Error: no command given\n");
+		printf("Usage: %s <command> [<default> <prompt>]\n", argv[0]);
+		return 1;
 	}
 
-	printf("REPL!");
+	if (argc > 5) {
+		printf("Error: too many commands given\n");
+		printf("Usage: %s <command> [<default> <prompt>]\n", argv[0]);
+		return 1;
+	}
+
+	char *command = argv[1];
+	char *default_command;
+	char *prompt_command;
+
+	bool prompt_given = false;
+	bool default_given = false;
+	bool sub_command_given = false;
+
+	char sub_command[BUF_SIZE];
+	char prompt[BUF_SIZE] = "";
+	char full_command[BUF_SIZE];
+
+	if (argc >= 3) {
+		default_command = argv[2];
+		default_given = true;
+	}
+	if (argc == 4) {
+		prompt_command = argv[3];
+		prompt_given = true;
+	}
+
+	strcat(prompt, command); // segfault here
+
+	while (true) {
+		// calculate prompt
+		if (prompt_given) {
+		}
+
+		// display prompt
+		printf("%s> ", prompt);
+
+		// read user input
+		fgets(sub_command, BUF_SIZE, stdin);
+		sub_command[strcspn(sub_command, "\n")] = 0; // remove new line
+		sub_command_given = strlen(sub_command) > 1;
+
+		// noop if no command and no default provided
+		if (!default_given && !sub_command_given) {
+			continue;
+		}
+
+		// build full command
+		strcpy(full_command, command);
+		strcat(full_command, " ");
+
+		if (sub_command_given) {
+			strcat(full_command, sub_command);
+		} else {
+			strcat(full_command, default_command);
+		}
+
+		// execute sub command, using default if no cmd provided
+		system(full_command);
+	}
 
 	return 0;
 }
